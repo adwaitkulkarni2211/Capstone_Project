261cddd25fbcc7d0e28fe094bd6ea1b8342bda1b
AmeyaKulkarni2001 <ameya.2000k@gmail.com>
2023-03-05 19:00:10 +0530

    View earlier commit message

--- CBR/main.py
+++ CBR/main.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import pymongo
+import os
+from dotenv import load_dotenv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-ds = pd.read_csv("./new_data.csv")
+load_dotenv()
+
+# ds = pd.read_csv("./new_data.csv")
+mongo_uri = os.getenv('MONGO_URI')
+
+client = pymongo.MongoClient(mongo_uri)
+db = client["TripPlanner"]
+
+places_data = db["new_places"].find()
+
+ds = pd.DataFrame(list(places_data))
 
 tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
-tfidf_matrix = tf.fit_transform(ds['description'])
+tfidf_matrix = tf.fit_transform(ds['features__properties__kinds'])
 
 cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
@@ -13,14 +26,14 @@ results = {}
 
 for idx, row in ds.iterrows():
     similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
-    similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
+    similar_items = [(cosine_similarities[idx][i], ds['features__id'][i]) for i in similar_indices]
 
-    results[row['id']] = similar_items[1:]
+    results[row['features__id']] = similar_items[1:]
     
-print('done!')
+# print('done!')
 
 def item(id):
-    return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
+    return ds.loc[ds['features__id'] == id]['features__properties__kinds'].tolist()[0]
 
 # Just reads the results out of the dictionary.
 def recommend(item_id, num):
@@ -30,4 +43,4 @@ def recommend(item_id, num):
     for rec in recs:
         print("Recommended: " + item(rec[1]) + " (score:" + str(rec[0]) + ")")
 
-recommend(item_id=11, num=5)
+recommend(item_id=1880355, num=5)
