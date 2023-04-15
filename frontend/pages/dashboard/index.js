1ddde81a5934b768da3d076d3d69dab62236d5d2
AmeyaKulkarni2001 <ameya.2000k@gmail.com>
2023-04-15 11:57:00 +0530

    Added loading spinner

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -14,6 +14,7 @@ import Stack from "@mui/material/Stack";
 import checkAuth from "@/components/checkAuth/checkAuth";
 import { signout } from "@/api/authAPICalls";
 import { useRouter } from "next/router";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -27,6 +28,7 @@ const dashboard = () => {
   const userid = 1;
   const [rec, setRec] = useState([]);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRec = () => {
@@ -58,6 +60,7 @@ const dashboard = () => {
             !prevData.some((prevItem) => prevItem.placeId === item.placeId)
         ),
       ]);
+      setLoading(false);
     };
     if (rec.length > 0) {
       fetchData();
@@ -74,57 +77,65 @@ const dashboard = () => {
 
   return (
     <>
-      <Button variant="contained" onClick={handleSignout}>
-        Sign Out
-      </Button>
-      {/* {console.log(data)} */}
-      <div style={{ paddingRight: "15rem", paddingLeft: "15rem" }}>
-        <Box sx={{ minWidth: 275 }}>
-          <Grid
-            container
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-            {data.map((item, index) => (
-              <Grid item xs={2} sm={4} md={4} key={index}>
-                {/* {console.log(item)} */}
-                <Item>
-                  <Card variant="outlined">
-                    {" "}
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        {item.placeData.features__properties__name}
-                      </Typography>
-                      <Stack spacing={1} alignItems="center">
-                        <Stack direction="row" spacing={1}>
-                          {/* <Chip label={item.placeData.features__properties__kinds} color="success" size="small"/> */}
-                          {item.placeData.features__properties__kinds
-                            .split(",")
-                            .map((kinds) => (
-                              <Chip
-                                label={kinds}
-                                color="success"
-                                size="small"
-                              />
-                            ))}
-                        </Stack>
-                      </Stack>
-                      <Typography variant="body2">
-                        well meaning and kindly.
-                        <br />
-                        {'"a benevolent smile"'}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Item>
-              </Grid>
-            ))}
-          </Grid>
+      {loading ? (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
         </Box>
-      </div>
+      ) : (
+        <div>
+          <Button variant="contained" onClick={handleSignout}>
+            Sign Out
+          </Button>
+          {/* {console.log(data)} */}
+          <div style={{ paddingRight: "15rem", paddingLeft: "15rem" }}>
+            <Box sx={{ minWidth: 275 }}>
+              <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+              >
+                {data.map((item, index) => (
+                  <Grid item xs={2} sm={4} md={4} key={index}>
+                    {/* {console.log(item)} */}
+                    <Item>
+                      <Card variant="outlined">
+                        {" "}
+                        <CardContent>
+                          <Typography variant="h5" component="div">
+                            {item.placeData.features__properties__name}
+                          </Typography>
+                          <Stack spacing={1} alignItems="center">
+                            <Stack direction="row" spacing={1}>
+                              {/* <Chip label={item.placeData.features__properties__kinds} color="success" size="small"/> */}
+                              {item.placeData.features__properties__kinds
+                                .split(",")
+                                .map((kinds) => (
+                                  <Chip
+                                    label={kinds}
+                                    color="success"
+                                    size="small"
+                                  />
+                                ))}
+                            </Stack>
+                          </Stack>
+                          <Typography variant="body2">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <Button size="small">Learn More</Button>
+                        </CardActions>
+                      </Card>
+                    </Item>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </div>
+        </div>
+      )}
     </>
   );
 };
