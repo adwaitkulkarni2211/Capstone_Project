0faffcfd1c766b49588926354793a5c126e766b5
Adwait Kulkarni <adwaitkulkarni2211@gmail.com>
2023-05-01 10:08:49 +0530

    getTripById and isAuthenticatedToEnterTrip

--- Node_API/controllers/trip.js
+++ Node_API/controllers/trip.js
@@ -1,6 +1,43 @@
 const Trip = require("../models/trip");
 const mongoose = require("mongoose");
 
+exports.getTripById = async (req, res, next, id) => {
+  Trip.findById(id)
+    .then((trip) => {
+      if (!trip) {
+        throw "trip not found in DB";
+      }
+      req.trip = trip;
+      next();
+    })
+    .catch((err) => {
+      if (err) {
+        return res.status(400).json({
+          error: "trip not found in DB",
+        });
+      }
+    });
+};
+
+exports.isAuthenticatedToEnterTrip = async (req, res) => {
+  try {
+    const userFound = req.trip.members.includes(req.profile._id);
+    if (userFound) {
+      res.json({
+        userid: req.profile._id,
+        trip: req.trip._id,
+      });
+    } else {
+      throw "NOT AUTHENTICATED TO ENTER TRIP";
+    }
+  } catch (error) {
+    console.log("NOT AUTHENTICATED TO ENTER TRIP", error);
+    return res.status(400).json({
+      error: error,
+    });
+  }
+};
+
 exports.createTrip = async (req, res) => {
   try {
     const newTrip = new Trip({
--- Node_API/routes/trip.js
+++ Node_API/routes/trip.js
@@ -2,17 +2,30 @@ const express = require("express");
 const router = express.Router();
 const { getUserById } = require("../controllers/user");
 const { getPlaceById } = require("../controllers/new_places");
-const { createTrip } = require("../controllers/trip");
+const {
+  createTrip,
+  getTripById,
+  isAuthenticatedToEnterTrip,
+} = require("../controllers/trip");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
 router.param("adminid", getUserById);
+router.param("userid", getUserById);
 router.param("placeid", getPlaceById);
+router.param("tripid", getTripById);
 
 router.post(
   "/trip/:adminid/:placeid/createTrip",
   isSignedIn,
   isAuthenticated,
   createTrip
+);
+
+router.get(
+  "/trip/:userid/:tripid",
+  isSignedIn,
+  isAuthenticated,
+  isAuthenticatedToEnterTrip
 );
 
 module.exports = router;
