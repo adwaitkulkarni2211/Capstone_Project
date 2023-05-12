64232982c1365624bf5ad5dbcc2e39dc7622624c
AmeyaKulkarni2001 <ameya.2000k@gmail.com>
2023-05-12 14:20:37 +0530

    Place search working / Getting User Data, not showing / Solved Merge err

--- frontend/components/navbar/navbar.js
+++ frontend/components/navbar/navbar.js
@@ -18,30 +18,54 @@ import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import LuggageIcon from '@mui/icons-material/Luggage';
-import PersonPinCircleIcon from '@mui/icons-material/PersonPinCircle';
+import LuggageIcon from "@mui/icons-material/Luggage";
+import PersonPinCircleIcon from "@mui/icons-material/PersonPinCircle";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Link from "next/link";
 
-
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const drawerWidth = 240;
-const navLinks = ["dashboard","trips","visitedplaces"];
+const navLinks = ["dashboard", "trips", "visitedplaces"];
 const Navbar = () => {
+  let jwt = "";
+
+  if (typeof window !== "undefined") {
+    jwt = JSON.parse(localStorage.getItem("jwt"));
+  }
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [option,setOption] = React.useState(["place"]);
+  const [option, setOption] = React.useState(["place"]);
+  const [place, setPlace] = React.useState();
+  const [result, setResult] = React.useState([]);
 
   const Search = styled("div")(({ theme }) => ({
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25)
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: "auto"
-    }
+      width: "auto",
+    },
   }));
 
   const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -51,7 +75,7 @@ const Navbar = () => {
     pointerEvents: "none",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   }));
 
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -65,20 +89,68 @@ const Navbar = () => {
       [theme.breakpoints.up("sm")]: {
         width: "12ch",
         "&:focus": {
-          width: "20ch"
-        }
-      }
-    }
+          width: "20ch",
+        },
+      },
+    },
   }));
 
   const handleChange = (event, newOption) => {
-    console.log(newOption)
+    console.log(newOption);
     setOption(newOption);
   };
 
-
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
+  };
+
+  const enterPressed = (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      const handleSearchPlace = async () => {
+        try {
+          const response = await fetch(`http://127.0.0.1:5000/search/${place}`);
+          const data = await response.json();
+          setResult(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      const searchPeople = async () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${jwt.token}`);
+    
+        var raw = JSON.stringify({
+          name: `${place}`,
+        });
+    
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow",
+        };
+    
+        try {
+          const response = await fetch(
+            `http://localhost:3000/api/user/${jwt.user._id}/getUsersByName`,
+            requestOptions
+          );
+          const result = await response.json();
+          setResult(result.users.users);
+          console.log(result);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+
+      setOpen(true);
+      if(option === "place") {handleSearchPlace()} else searchPeople()
+    }
+  };
+  const addPeople = (name, email, _id) => {
+    
   };
 
   const drawer = (
@@ -91,9 +163,9 @@ const Navbar = () => {
         {navLinks.map((item) => (
           <ListItem key={item} disablePadding>
             <a href={`/${item}`}>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
             </a>
           </ListItem>
         ))}
@@ -101,10 +173,8 @@ const Navbar = () => {
     </Box>
   );
 
-
-
   return (
-    <Box sx={{ display: "flex" ,justifyContent:"space-between" }}>
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -124,56 +194,122 @@ const Navbar = () => {
           >
             TripTravellor
           </Typography>
-          <Box sx={{ ml: 8,display : "flex"}}>
-            <Search>
+          <Box sx={{ ml: 8, display: "flex" }}>
+            {/* <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
               <StyledInputBase
                 placeholder="Search…"
                 inputProps={{ "aria-label": "search" }}
+                onChange={(e) => setPlace(e.target.value)}
+                value={place}
+                
               />
-            </Search>
-            <ToggleButtonGroup
-                value={option}
-                exclusive
-                onChange={handleChange}
-                aria-label="text formatting"
+            </Search> */}
+            <TextField
+              label="Search ..."
+              onChange={(e) => setPlace(e.target.value)}
+              value={place}
+              onKeyDown={enterPressed}
+            />
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
             >
-                <ToggleButton value="place" aria-label="bold" defaultChecked>
-                <LuggageIcon/>  
-                </ToggleButton>
-                <ToggleButton value="person">
-                <PersonPinCircleIcon/>
-                </ToggleButton>
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Search {option === place ? "Place" : "People"} Result
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  {option === "place" && result.length > 0 && (
+                    <ul className="search-results">
+                      {result.map((result) => (
+                        <li key={result.features__id}>
+                          {result.features__properties__name}
+                          <Link
+                            href={{
+                              pathname: "/place",
+                              query: {
+                                id: result.features__id,
+                                name: result.features__properties__name,
+                                kinds: result.features__properties__kinds,
+                                coordinate1:
+                                  result.features__geometry__coordinates__001,
+                                coordinate2:
+                                  result.features__geometry__coordinates__002,
+                              },
+                            }}
+                          >
+                            <Button size="small">Learn More</Button>
+                          </Link>
+                        </li>
+                      ))}
+                      {option === "person" && result &&
+                        result.map(({ name, email, _id }) => (
+                          <div>
+                            <Typography
+                              key={_id}
+                            >{`name: ${name} & email: ${email}`}</Typography>
+                            <Button onClick={() => addPeople(name, email, _id)}>
+                              add
+                            </Button>
+                          </div>
+                        ))}
+                    </ul>
+                  )}
+                  .
+                </Typography>
+              </Box>
+            </Modal>
+            <ToggleButtonGroup
+              value={option}
+              exclusive
+              onChange={handleChange}
+              aria-label="text formatting"
+            >
+              <ToggleButton value="place" aria-label="bold" defaultChecked>
+                <LuggageIcon />
+              </ToggleButton>
+              <ToggleButton value="person">
+                <PersonPinCircleIcon />
+              </ToggleButton>
             </ToggleButtonGroup>
-            </Box>
-          <Box sx={{ ml : 10,display: { xs: "none", sm: "block" }, alignItems:"center",justifyContent:"center"}}>
+          </Box>
+          <Box
+            sx={{
+              ml: 10,
+              display: { xs: "none", sm: "block" },
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             {navLinks.map((item) => (
               <a key={item} href={`/${item}`}>
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
+                <Button key={item} sx={{ color: "#fff" }}>
+                  {item}
+                </Button>
               </a>
             ))}
           </Box>
-          
-                </Toolbar>
-            </AppBar>
+        </Toolbar>
+      </AppBar>
       <Box component="nav">
         <Drawer
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true // Better open performance on mobile.
+            keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
             display: { xs: "block", sm: "none" },
             "& .MuiDrawer-paper": {
               boxSizing: "border-box",
-              width: drawerWidth
-            }
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -181,8 +317,6 @@ const Navbar = () => {
       </Box>
     </Box>
   );
-}
-
-
+};
 
 export default Navbar;
